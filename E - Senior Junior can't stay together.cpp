59b44afeb660d9d4be744bf9d0e14d626b713b7e
Yeasir Arafat Atik <122221300+Atik058@users.noreply.github.com>
2026-09-02 12:03:40 +0600

    Add main function to handle input and output

---
+++ E - Senior Junior can't stay together.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int n;
+    cin>>n;
+    if(n>47 && n<51)
+        cout<<"Probin";
+    else if(n>50 && n<54)
+        cout<<"Nobin";
+    else
+        cout<<"Bohiragoto";
+}
