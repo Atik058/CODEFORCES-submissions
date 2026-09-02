4b24cb62b0318ccc1ce01bf38e7425c4ae395177
Yeasir Arafat Atik <122221300+Atik058@users.noreply.github.com>
2026-09-02 20:40:34 +0600

    Create 	A - Add Odd or Subtract Even.cpp

---
+++ A - Add Odd or Subtract Even.cpp
@@ -0,0 +1,45 @@
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin>>t;
+    int a,b;
+    while(t--)
+    {
+        cin>>a>>b;
+        if(a==b)
+        {
+            cout<<0<<endl;
+        }
+        else if(a>b)
+        {
+            if((a+b)%2==0)
+            {
+                cout<<1<<endl;
+            }
+            else
+            {
+                cout<<2<<endl;
+            }
+        }
+        else
+        {
+            if((a+b)%2==0)
+            {
+                cout<<2<<endl;
+            }
+            else
+            {
+                cout<<1<<endl;
+            }
+            
+        }
+        
+    }
+    
+    
+ 
+    return 0;
+}
